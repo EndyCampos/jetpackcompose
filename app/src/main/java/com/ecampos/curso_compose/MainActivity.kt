9922e452a97a8348c0841ad02e83bcebd69fdf99
ecampos <endycv@gmail.com>
2025-06-26 16:53:19 -0700

    feat: implementación de GridCell con distribución usando Modifier.weight y colores personalizados

--- app/src/main/java/com/ecampos/curso_compose/MainActivity.kt
+++ app/src/main/java/com/ecampos/curso_compose/MainActivity.kt
@@ -4,27 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.ecampos.curso_compose.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,14 +33,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    GreetingImage(
-                        message1 = stringResource(R.string.all_tasks_completed),
-                        message2 = stringResource(R.string.nice_work)
-                    )
-
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GridExample()
                 }
             }
         }
@@ -48,42 +45,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingImage(message1: String, message2: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.circle)
-    Column ( modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center ) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.size(200.dp)
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Text(
-            text = message1,
-            fontSize = 24.sp,
-            textAlign = TextAlign.Center,
-
+fun GridExample() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row (modifier = Modifier.weight(1f)) {
+            GridCell(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = Modifier.weight(1f),
+                backgroundColor = colorResource(id = R.color.color1)
             )
+            GridCell(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = Modifier.weight(1f),
+                backgroundColor = colorResource(id = R.color.color2)
+            )
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            GridCell(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                modifier = Modifier.weight(1f),
+                backgroundColor = colorResource(id = R.color.color3)
+            )
+            GridCell(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                modifier = Modifier.weight(1f),
+                backgroundColor = colorResource(id = R.color.color4)
+            )
+        }
+    }
+}
+
+@Composable
+fun GridCell(title: String, description: String, modifier: Modifier, backgroundColor: Color) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = message2,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 8.dp)
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Justify
         )
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(
-            message1 = stringResource(R.string.all_tasks_completed),
-            message2 = stringResource(R.string.nice_work)
-        )
+        GridExample()
     }
 }
+
